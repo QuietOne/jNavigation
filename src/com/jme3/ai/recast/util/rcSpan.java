19fd3bafc6fa9056c3695ddd0d1c080941bca380
QuietOne <tihomir.radosavljevic@gmail.com>
2014-05-19 17:46:08 +0200

    added span

---
+++ src/com/jme3/ai/recast/util/rcSpan.java
@@ -0,0 +1,32 @@
+package com.jme3.ai.recast.util;
+
+/**
+ * Represents a span in a heightfield.
+ *
+ * @author Tihomir Radosavljevic
+ * @version 0.1
+ */
+public class rcSpan {
+
+    /**
+     * WARNING: unsigned The area id assigned to the span.
+     */
+    public int area;
+
+    /**
+     * The next span higher up in column.
+     */
+    public rcSpan next;
+
+    /**
+     * WARNING: unsigned The upper limit of the span. [Limit: <=
+     * RC_SPAN_MAX_HEIGHT].
+     */
+    public int smax;
+
+    /**
+     * WARNING: unsigned The lower limit of the span. [Limit: < smax].
+     */
+    public int smin;
+
+}
