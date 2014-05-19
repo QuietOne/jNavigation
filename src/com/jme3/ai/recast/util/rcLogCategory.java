96c65709cfa4bf5ce5fbd48a6f027d932b217b30
QuietOne <tihomir.radosavljevic@gmail.com>
2014-05-19 17:45:51 +0200

    added log category enum

---
+++ src/com/jme3/ai/recast/util/rcLogCategory.java
@@ -0,0 +1,23 @@
+package com.jme3.ai.recast.util;
+
+/**
+ * Recast log categories.
+ *
+ * @author Tihomir Radosavljevic
+ * @version 0.1
+ */
+public enum rcLogCategory {
+
+    /**
+     * A progress log entry.
+     */
+    RC_LOG_PROGRESS,
+    /**
+     * A warning log entry.
+     */
+    RC_LOG_WARNING,
+    /**
+     * An error log entry.
+     */
+    RC_LOG_ERROR
+}
