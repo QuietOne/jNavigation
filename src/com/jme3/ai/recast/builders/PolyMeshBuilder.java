690b48c0a14383a8494018ed070c1296f5b72ffb
QuietOne <tihomir.radosavljevic@gmail.com>
2014-05-28 15:24:55 +0200

    added javadoc

--- src/com/jme3/ai/recast/builders/PolyMeshBuilder.java
+++ src/com/jme3/ai/recast/builders/PolyMeshBuilder.java
@@ -4,8 +4,11 @@ import com.jme3.ai.recast.structures.ContourSet;
 import com.jme3.ai.recast.structures.PolyMesh;
 
 /**
+ * PolyMeshBuilder is class for correct manipulation on PolyMesh
+ * structure.
  *
  * @author Tihomir Radosavljevic
+ * @version 1.0
  */
 public class PolyMeshBuilder extends Builder {
 
@@ -16,18 +19,33 @@ public class PolyMeshBuilder extends Builder {
         polyMesh = new PolyMesh();
     }
     
+    /**
+     * 
+     * @param contourSet
+     * @param numberOfVerticesPerPoly
+     * @return 
+     */
     public boolean buildPolyMesh(ContourSet contourSet, int numberOfVerticesPerPoly){
         return rcBuildPolyMesh(contourSet, numberOfVerticesPerPoly);
     }
 
     private native boolean rcBuildPolyMesh(ContourSet cset, int nvp);
 
+    /**
+     * 
+     * @param polyMeshes
+     * @return 
+     */
     public boolean addPolyMeshes(PolyMesh[] polyMeshes){
         return rcMergePolyMeshes(polyMeshes);
     }
     
     private native boolean rcMergePolyMeshes(PolyMesh[] meshes);
 
+    /**
+     * 
+     * @return 
+     */
     public PolyMesh getPolyMesh() {
         return polyMesh;
     }
