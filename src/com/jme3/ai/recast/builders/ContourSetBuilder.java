22ac86b833290cfda1aeafed5d05bd73f1714998
QuietOne <tihomir.radosavljevic@gmail.com>
2014-05-28 15:23:54 +0200

    added javadoc

--- src/com/jme3/ai/recast/builders/ContourSetBuilder.java
+++ src/com/jme3/ai/recast/builders/ContourSetBuilder.java
@@ -4,8 +4,11 @@ import com.jme3.ai.recast.structures.CompactHeightfield;
 import com.jme3.ai.recast.structures.ContourSet;
 
 /**
+ * ContourSetBuilder is class for correct manipulation on ContourSet
+ * structure.
  *
  * @author Tihomir Radosavljevic
+ * @version 1.0
  */
 public class ContourSetBuilder extends Builder {
 
@@ -16,12 +19,24 @@ public class ContourSetBuilder extends Builder {
         contourSet = new ContourSet();
     }
 
+    /**
+     * 
+     * @param compactHeightfield
+     * @param maxError
+     * @param maxEdgeLength
+     * @param buildFlags
+     * @return 
+     */
     public boolean buildContours(CompactHeightfield compactHeightfield, float maxError, int maxEdgeLength, int buildFlags){
         return rcBuildContours(compactHeightfield, maxError, maxEdgeLength, buildFlags);
     }
     
     private native boolean rcBuildContours(CompactHeightfield chf, float maxError, int maxEdgeLen, int buildFlags);
 
+    /**
+     * 
+     * @return 
+     */
     public ContourSet getContourSet() {
         return contourSet;
     }
