8d8a1bc1db9c72e43304d5e0f7953845b5b903af
QuietOne <tihomir.radosavljevic@gmail.com>
2014-05-28 12:57:52 +0200

    final touches

--- src/com/jme3/ai/recast/structures/BoundedField.java
+++ src/com/jme3/ai/recast/structures/BoundedField.java
@@ -6,6 +6,7 @@ import com.jme3.math.Vector3f;
  * Base class for all other Recast structures.
  *
  * @author Tihomir Radosavljevic
+ * @version 1.0
  */
 public class BoundedField {
 
@@ -37,26 +38,12 @@ public class BoundedField {
 
     private native void getNativeMaxBound();
 
-    public void setMaxBound(Vector3f maxBound) {
-        this.maxBound = maxBound;
-        setMaxBound();
-    }
-
-    private native void setMaxBound();
-
     public Vector3f getMinBound() {
         getNativeMinBound();
         return minBound;
     }
 
     private native void getNativeMinBound();
-
-    public void setMinBound(Vector3f minBound) {
-        this.minBound = minBound;
-        setMinBound();
-    }
-
-    private native void setMinBound();
 
     public float getCellHeight() {
         getNativeCellHeight();
@@ -65,24 +52,10 @@ public class BoundedField {
 
     private native void getNativeCellHeight();
 
-    public void setCellHeight(float cellHeight) {
-        this.cellHeight = cellHeight;
-        setCellHeight();
-    }
-
-    private native void setCellHeight();
-
     public float getCellSize() {
         getNativeCellSize();
         return cellSize;
     }
 
     private native void getNativeCellSize();
-
-    public void setCellSize(float cellSize) {
-        this.cellSize = cellSize;
-        setCellSize();
-    }
-
-    private native void setCellSize();
 }
